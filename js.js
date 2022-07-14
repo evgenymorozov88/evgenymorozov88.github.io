d91916d3403a356d3f3293057f81eddb2752a3f1
evgenymorozov88 <109293855+evgenymorozov88@users.noreply.github.com>
2022-07-14 22:00:23 +0300

    Файлы для сайта

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('YOU HAVE THE RIGHT TO CHOOSE YOUR OWN DEAD-END JOB!')
+})
